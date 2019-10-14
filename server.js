21046fc9cadd9945451d8c524c0c9a96b021b112
Randoomed_ <nico_fr1110@hotmail.com>
2019-10-14 13:37:02 -0400

    test9

--- server.js
+++ server.js
@@ -2,10 +2,8 @@
 const express = require('express');
 const app = express();// Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));// Start the app by listening on the default
+app.use(express.static(__dirname + '/src/app'));// Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+app.listen(process.env.PORT || 3000 );
 
 //app.get('*', routes.index);
