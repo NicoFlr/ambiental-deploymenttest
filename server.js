3d2449b0cfa1cd51d5547e353be4fce75d82a154
Randoomed <nico_fr1110@hotmail.com>
2019-10-15 22:33:44 -0400

    test12

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 // server.js
 const express = require('express');
+const path = require('path');
 const app = express();// Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));// Start the app by listening on the default
+//app.use(express.static(__dirname + '/dist'));// Start the app by listening on the default
 // Heroku port
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+//app.get('/*', function(req, res) {
+  //  res.sendFile(path.join(__dirname + '/dist/index.html'));
+//});
 
-app.listen(process.env.PORT || 8080);
+//app.listen(process.env.PORT || 8080);
 
 //app.get('*', routes.index);
+//Install express server
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/ambiental-deploymenttest'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/ambiental-deploymenttest/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
